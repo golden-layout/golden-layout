994c7a2a0bf412c7f4d20661d391d9be6e575845
Wolfram Hempel <hempel.wolfram@gmail.com>
2014-10-11 18:34:43 +0100

    Added release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,27 @@ module.exports = function(grunt) {
 			tasks: [ 'build' ],
 			files: [ './src/**' ],
 			options: { livereload: 5051 },
+		},
+
+		/***********************
+		* WATCH
+		***********************/
+		release: {
+			options: {
+			tagName: 'v<%= version %>',
+			github: { 
+				repo: 'hoxton-one/golden-layout',
+				usernameVar: 'GITHUB_USERNAME',
+				passwordVar: 'GITHUB_PASSWORD'
+			}
 		}
+  	}
 
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-release');
 
 	// Default task(s).
 	grunt.registerTask('default', ['watch']);
