99c32ea5e494c02307e1b9476abead48d2bab04b
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-03-20 18:34:10 +0100

    Basic homepage structure

--- htdocs/assets/js/startpage.js
+++ htdocs/assets/js/startpage.js
@@ -7,8 +7,8 @@ if (!Function.prototype.bind) {
       throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
     }
 
-    var aArgs = Array.prototype.slice.call(arguments, 1), 
-        fToBind = this, 
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
         fNOP = function () {},
         fBound = function () {
           return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
@@ -43,15 +43,13 @@ if (!Function.prototype.bind) {
 									{
 										type: 'component',
 										title: 'Golden',
-										componentName: 'demo',
-										width: 30,
-										componentState: { bg: 'golden_layout_spiral.png' }
+										componentName: 'fibonacci-spiral',
+										width: 30
 									},
 									{
 										title: 'Layout',
 										type: 'component',
-										componentName: 'demo',
-										componentState: { bg: 'golden_layout_text.png' }
+										componentName: 'gl-text'
 									}
 								]
 							},
@@ -67,7 +65,7 @@ if (!Function.prototype.bind) {
 										}
 									},
 									{
-										
+
 										type: 'component',
 										title: 'LexCorp plc.',
 										componentName: 'stockChart',
@@ -111,12 +109,23 @@ if (!Function.prototype.bind) {
 
 	var myLayout = new GoldenLayout( config, '#exampleLayoutContainer' );
 
-	myLayout.registerComponent( 'demo', function( container, state ) {
-		if( state.bg ) {
-			container
-				.getElement()
-				.append( '<img class="testImg" src="assets/images/' + state.bg + '" />' );
-		}
+	myLayout.registerComponent( 'fibonacci-spiral', function( container, state ) {
+		container
+			.getElement()
+			.append( '<div class="fibonacci-spiral"></div>' );
+	});
+
+	myLayout.registerComponent( 'gl-text', function( container, state ) {
+		container
+			.getElement()
+			.append(
+				'<div class="gl-text">' +
+					'<div>' +
+						'<h1>GoldenLayout</h1>' +
+						'<h3>a multi-screen layout manager for webapps</h3>' +
+					'</div>' +
+				'</div>'
+			);
 	});
 
 	/****************************************
@@ -190,13 +199,13 @@ if (!Function.prototype.bind) {
 	* **************************************/
 	myLayout.registerComponent( 'pieChart', function( container, state ){
 		config = {
-	        title: { 
+	        title: {
 	        	text: 'Market Share',
 	        	style: { 'color': '#555', 'fontSize': '14px' }
 	        },
 	        legend:{ enabled: false },
 	        credits:{ enabled: false },
-	        plotOptions:  { 
+	        plotOptions:  {
 	        	pie: {
 	        		colors: [ '#4A4A4A', '#444444', '#3A3A3A' ],
 	                allowPointSelect: true,
@@ -228,7 +237,7 @@ if (!Function.prototype.bind) {
 	                ['Competitor E', 3.9 ]
 	            ]
 	        }],
-	        chart:{ 
+	        chart:{
 	        	renderTo: container.getElement()[ 0 ],
 
 	        	backgroundColor: '#222'
@@ -243,26 +252,26 @@ if (!Function.prototype.bind) {
 	* **************************************/
 	myLayout.registerComponent( 'columnChart', function( container, state ){
 		config = {
-	        title: { 
+	        title: {
 	        	text: 'Relative Performance',
 	        	style: { 'color': '#555', 'fontSize': '14px' }
 	        },
 	        legend:{ enabled: false },
 	        credits:{ enabled: false },
-	        plotOptions:  { 
-	        	
+	        plotOptions:  {
+
             },
-	        xAxis: { 
-	        	categories: ['Q1','Q2','Q3','Q4'], 
-	        	gridLineColor: '#3A3A3A', 
-	        	lineColor: '#3A3A3A', 
+	        xAxis: {
+	        	categories: ['Q1','Q2','Q3','Q4'],
+	        	gridLineColor: '#3A3A3A',
+	        	lineColor: '#3A3A3A',
 	        	tickColor:'#3A3A3A' },
 	        yAxis: {
 	            min: 0,
 	            title: { text: null },
 	            labels: { enabled: false },
-	            gridLineColor: '#3A3A3A', 
-	            lineColor: '#3A3A3A', 
+	            gridLineColor: '#3A3A3A',
+	            lineColor: '#3A3A3A',
 	            tickColor:'#3A3A3A'
 	        },
 	         series: [{
@@ -271,7 +280,7 @@ if (!Function.prototype.bind) {
 	         	name: 'rel. performance',
 	            data: [49.9, 71.5, 26, 33]
 	        }],
-	        chart:{ 
+	        chart:{
 	        	renderTo: container.getElement()[ 0 ],
 	        	type: 'column',
 	        	backgroundColor: '#222'
@@ -310,21 +319,21 @@ if (!Function.prototype.bind) {
 	StockGridComponent.prototype._scheduleGridCreation = function() {
 	  var interval = setInterval(function(){
 	    var stylesheetNodes = $('link[rel=stylesheet]'), i;
-	    
+
 	    for( i = 0; i < stylesheetNodes.length; i++ ) {
 	      if( stylesheetNodes[ i ].sheet === null ){
 	        return;
 	      }
 	    }
-	    
+
 	    clearInterval( interval );
 	    this._createGrid();
-	    
+
 	  }.bind( this ), 10 );
 	};
 
 	StockGridComponent.prototype._createGrid = function() {
-	  this._grid = new Slick.Grid( 
+	  this._grid = new Slick.Grid(
 	    this._container.getElement(),
 	    (function(){
 	    var data = ['Acme, inc.','ACI','Ajax','AJX','Allied Biscuit','ABS','Ankh-Sto Associates','ASA','Atlantic Northern','ANO','Axis Chemical Co.','ACC','Barrytron','BRT','Big Kahuna Burger','BKB','Big T Burgers and Frie','BBF','Blammo','BLM','BLAND Corporation','BLC','Bluth Company','BLU','Burleigh and Strongint','BAS','C.H. Lavatory and Sons','CLS','Carrys Candles','CCD','Central Perk','CEP','Charles Townsend Agenc','CTA','Chasers','CSS','Chez Quis','CQU','Chotchkies','CKI','Cogswell Cogs','CCO','Colonial Movers','CMO','Compuglobalhypermegane','CCH','Corellian Engineering','CEN','Data Systems','DAS','Duff Brewing Company','DBC','Dunder Mifflin','DMF','Extensive Enterprise','EEP','Fake Brothers','FBR','Flowers By Irene','FBI','Foo Bars','FOB','Gadgetron','GAG','Galaxy Corp','GAC','General Forge and Foun','GFF','General Products','GPO','General Services Corpo','GSC','Gizmonic Institute','GIT','Globex Corporation','GLC','Globo Gym American Cor','GGA','Globo-Chemimacals','GCE','Gringotts','GGO','Incom Corporation','ICO','Industrial Automation','IAU','Initech','IIT','Initrode','INT','Input, Inc.','INP','Keedsler Motors','KMS','Klimpys','KLM','Krustyco','KUC','Kumatsu Motors','KMO','Leeding Engines Ltd.','LEL','LexCorp','LCO','LuthorCorp','LCP','Mainway Toys','MTO','Mammoth Pictures','MPI','McMahon and Tate','MAT','Megadodo Publications','MPU','Milliways','MIW','Minuteman Cafe','MIC','Moes Tavern','MOT','Monarch Playing Card Corp','MPC','Monks Diner','MOD','Mooby Corp','MOB','Mr. Sparkle','MRS','Niagular','NIA','Nordyne Defense Dynamimics','NDD','North Central Positron','NCP','Omni Consimer Products','OCP','Osato Chemicals','OCE','Petrox Oil Company','POC','Plow King','PLK','Powell Motors','PWM','Praxis Corporation','PRX','Primatech','PMT','QWERTY Logistics','QWR','Roboto Industries','ROI','Rouster and Sideways','RAS','Roxxon','ROX','Sirius Cybernetics Cor','SCC','Sixty Second Avenue','SSA','Smith and Co.','SAC','Sombra Corporation','SOC','Sonky Rubber Goods','SRG','Spacely Sprockets','SPR','Spade and Archer','SAA','SpringShield','SSH','St. Anky Beer','SAB','Stay Puft Corporation','SPC','Sto Plains Holdings','SPH','Strickland Propane','SPO','Taco Grande','TAG','Taggart Transcontinent','TTA','Tessier-Ashpool','TEA','Thatherton Fuels','TAF','The Frying Dutchman','TFD','The Krusty Krab','TKK','The Legitimate Busines','TLB','The New Firm','TNF','Three Waters','TWA','Thrift Bank','TBA','Tip Top Cafe','TTC','Transworld Consortium','TWC','U.S. Robotics and Mech','URM','United Fried Chicken','UFC','Universal Export','UEX','Vandelay Industries','VAI','Videlectrix','VEX','Virtucon','VUC','Water and Power','WAP','Wentworth Industries','WEI','Wernham Hogg','WEH','Western Gas & Electric','WGE','Widget Corp','WCO','Zevo Toys','ZET','ZiffCorp','ZFC'];
@@ -345,7 +354,7 @@ if (!Function.prototype.bind) {
 
 	    return output;
 	  })(),
-	 
+
 	    this._columns,
 	    this._options
 	  );
