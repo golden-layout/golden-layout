3cc08c467eace556b22c2493d90d6d191cdbb43a
Norbert de Langen <ndelangen@me.com>
2018-04-26 01:23:55 +0200

    CHANGE .eslint config to reflect current codebase state without errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,49 @@ const ignore = 0;
 module.exports = {
   extends: ['eslint-config-airbnb', 'plugin:jest/recommended', 'prettier'],
   plugins: ['prettier', 'jest', 'react', 'json'],
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jquery": true
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jquery': true
   },
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "sourceType": "module"
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'sourceType': 'module'
   },
-  "globals": {
-      "lm": warn
+  'globals': {
+      'lm': warn
   },  
-  "rules": {
-    "strict": ignore,
+  'rules': {
+    'block-scoped-var': warn,
+    'class-methods-use-this': warn,
+    'consistent-return': warn,
+    'func-names': ignore,
+    'guard-for-in': ignore,
+    'import/no-mutable-exports': warn,
+    'no-continue': warn,
+    'no-multi-assign': warn,
+    'no-new': warn
+    'no-param-reassign': [
+      'warn',
+      {
+        'props': true
+      }
+    ],
+    'no-plusplus': ignore,
+    'no-prototype-builtins': warn,
+    'no-restricted-syntax': warn,
+    'no-shadow': warn,
+    'no-undef-init': ignore,
+    'no-undef': ignore,
+    'no-undefined': ignore,
+    'no-underscore-dangle': ignore,
+    'no-unused-expressions': ignore,
+    'no-unused-vars': warn,
+    'no-use-before-define': warn,
+    'no-var': warn,
+    'prefer-destructuring': warn,
+    'prefer-rest-params': warn,
+    'prefer-spread': warn,
     'prettier/prettier': [
       warn,
       {
@@ -29,15 +58,7 @@ module.exports = {
         singleQuote: true,
       },
     ],
-    "no-undef": "off",
-    "no-undefined": "off",
-    "no-undef-init": "off",
-    "no-param-reassign": [
-      "warn",
-      {
-        "props": true
-      }
-    ],
-    "no-unused-vars": "warn"
+    'strict': ignore,
+    'vars-on-top': warn,
   }
 }
