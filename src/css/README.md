60fe3e8ff1ec4d409bf2e7dc21cc4080c3c9402e
Martin Koppehel <martin.koppehel@st.ovgu.de>
2020-02-12 18:32:49 +0100

    start cleanup the build process

--- src/css/README.md
+++
@@ -1,4 +0,0 @@
-# Beware!
-
- All these files are generated automatically from the `less` templates in `../src/less`
- and are created by the `grunt less` command.
--- src/my_traces.js
+++
@@ -1,9 +0,0 @@
-
-// Tracing.before('$', function(fnName, args, depth) { 
-//     if(navigator.userAgent.indexOf("Chrome") !== -1){
-//       var stack = new Error().stack
-//       console.info(fnName, '{' + depth + '} args:', args, ' at:', stack.split("\n").splice(3).join("\n"))
-//     } else {
-//       console.log(fnName, '{' + depth + '} args:', args)
-//     }    
-// })
