bbddd38a1f776f1d8f4cec4fca41d8058991f659
Martin Koppehel <martin.koppehel@st.ovgu.de>
2020-04-15 17:32:36 +0200

    Add autoprefixer to css build

--- scripts/css.js
+++ scripts/css.js
@@ -1,6 +1,8 @@
-const fs = require("fs")
-const path = require("path")
-const less = require("less")
+const fs = require("fs");
+const path = require("path");
+const less = require("less");
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
 
 // This ensures a directory. If the directory already exists, we do not do anything.
 const ensureFolder = (dir) => {
@@ -13,20 +15,24 @@ const ensureFolder = (dir) => {
 // Must be called for every .less file in src folder.
 const buildFile = async (filePath) => {
     const outputPath = filePath.replace("less", "css").replace("src", "dist").replace(".less", ".css")
-    const lessFile = fs.readFileSync(filePath, "utf8")
-    const lessOutput = await less.render(lessFile)
-    fs.writeFileSync(outputPath, lessOutput.css)
-}
+    const lessFile = fs.readFileSync(filePath, "utf8");
+    const lessOutput = await less.render(lessFile);
+    const prefixedOutput = await postcss([autoprefixer]).process(lessOutput.css, { from: filePath });
+    prefixedOutput.warnings().forEach(warn => {
+        console.warn(`[${filePath}] [WARN]: ${warn}`);
+    });
+    fs.writeFileSync(outputPath, prefixedOutput.css)
+};
 
 // We do not have mkdir -p without an extra dependency.
-ensureFolder("./dist")
-ensureFolder("./dist/css")
-ensureFolder("./dist/css/themes")
+ensureFolder("./dist");
+ensureFolder("./dist/css");
+ensureFolder("./dist/css/themes");
 
 // Build base.less file
-buildFile("./src/less/goldenlayout-base.less")
+buildFile("./src/less/goldenlayout-base.less");
 
 // Build every theme
 fs.readdirSync("./src/less/themes").forEach(file => {
-    buildFile(path.join(`./src/less/themes/`, file))
+    buildFile(path.join(`./src/less/themes/`, file));
 });
