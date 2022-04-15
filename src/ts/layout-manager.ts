cc3f3c75d3a501af7778dd2e33febcbf6b5e0460
Paul Klink <paul@klink.id.au>
2022-04-16 06:32:35 +1000

    Revert height in TabDropPlaceholder for backwards compatibility

--- src/ts/layout-manager.ts
+++ src/ts/layout-manager.ts
@@ -1763,6 +1763,7 @@ export namespace LayoutManager {
     /** @internal */
     export function createTabDropPlaceholderElement(document: Document): HTMLElement {
         const element = document.createElement('div');
+        element.style.height = 'revert';  // for backwards compatibility - remove in future
         element.classList.add(DomConstants.ClassName.DropTabPlaceholder);
         return element;
     }
