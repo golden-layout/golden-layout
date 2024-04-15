a1c645fb71493be34a047ac26bcd00786e0642fe
Landon Moss <landonmoss0729@gmail.com>
2024-04-15 10:38:03 -0400

    changes

--- test/disabled/tab-tests.js
+++ test/disabled/tab-tests.js
@@ -22,23 +22,30 @@ describe( 'tabs apply their configuration', function(){
 
 	it( 'attached a drag listener to the first tab', function(){
 		
+		var firstComponent = layout.root.contentItems[0].contentItems[0];
+        var secondComponent = layout.root.contentItems[0].contentItems[1];
+        var layoutHeader = layout.root.contentItems[0].header;
 
-		var item1 = layout.root.contentItems[ 0 ].contentItems[ 0 ],
-			item2 = layout.root.contentItems[ 0 ].contentItems[ 1 ],
-			header = layout.root.contentItems[ 0 ].header;
 
-		expect( header.tabs.length ).toBe( 2 );
 
-		expect( item1.type ).toBe( 'component' );
-		expect( item1.config.reorderEnabled ).toBe( true );
-		expect( header.tabs[ 0 ]._dragListener ).toBeDefined();
+		//var item1 = layout.root.contentItems[ 0 ].contentItems[ 0 ],
+		//	item2 = layout.root.contentItems[ 0 ].contentItems[ 1 ],
+		//	header = layout.root.contentItems[ 0 ].header;
 
-		expect( item2.type ).toBe( 'component' );
-		expect( item2.config.reorderEnabled ).toBe( false );
-		expect( header.tabs[ 1 ]._dragListener ).not.toBeDefined();
+		expect( layoutHeader.tabs.length ).toBe( 2 );
+
+		expect( firstComponent.type ).toBe( 'component' );
+		expect( firstComponent.config.reorderEnabled ).toBe( true );
+		expect( layoutHeader.tabs[ 0 ]._dragListener ).toBeDefined();
+
+		expect( secondComponent.type ).toBe( 'component' );
+		expect( secondComponent.config.reorderEnabled ).toBe( false );
+		expect( layoutHeader.tabs[ 1 ]._dragListener ).not.toBeDefined();
 	});
 
 	it( 'destroys the layout', function(){
 		layout.destroy();
 	});
 });
+
+//
